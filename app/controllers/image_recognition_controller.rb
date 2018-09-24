eb505c1abc13f5ce2cfff3da170fcbe9e1a222ec
Vincent Ly <veely1@gmail.com>
2018-09-24 07:23:17 +0000

    image upload working with watson

---
+++ app/controllers/image_recognition_controller.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'ibm_watson'
+
+class ImageRecognitionController < ApplicationController
+  def create
+    apikey = params[:apikey]
+    image = params[:image]
+    imageName = params[:imageName]
+    foodResult = nil
+
+    visual_recognition = IBMWatson::VisualRecognitionV3.new(
+      version: "2018-03-19",
+      iam_apikey: apikey
+    )
+
+    f = File.new('./public/images/' + imageName, 'wb')
+    f.write(Base64.decode64(image))
+    f.close
+
+    File.open('./public/images/' + imageName) do |image_file|
+      foodResult = visual_recognition.classify(
+        images_file: image_file,
+        threshold: 0.1
+      ).result
+      puts JSON.pretty_generate(foodResult)
+    end
+
+    respond_to do |format|
+      msg = { :status => "Success", :message => foodResult }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
+
+end
