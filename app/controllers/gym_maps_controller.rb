7cec7462f565c5c671d0ba9f398cd0121260a367
lexy5251 <xixi5251@gmail.com>
2018-09-27 19:24:24 +0000

    gym map

--- app/controllers/gym_maps_controller.rb
+++ app/controllers/gym_maps_controller.rb
@@ -5,11 +5,25 @@ require 'geocoder'
 class GymMapsController < ApplicationController
 
   def index
-    @client = GooglePlaces::Client.new('AIzaSyDJ27_9A1XNoXhVa8X5QnyHRD59-CF42I4')
-    maps = @client.spots(43.644758, -79.395274, :types => ['gym', 'health'], :exclude => 'store')
+
+    current_location = User.all.pluck(:location)
+    puts current_location
+
+
+    centerLat = Geocoder.search(current_location[0]).first.data["lat"].to_f
+    centerLng = Geocoder.search(current_location[0]).first.data["lon"].to_f
+
+     puts centerLat
+     puts centerLng
+     puts Geocoder.search(current_location[0])
+     puts Geocoder.search(current_location[0]).first
+     puts Geocoder.search(current_location[0]).first.data
+
+
+    @client = GooglePlaces::Client.new(ENV['GOOGLE_PLACES_API_TOKEN'])
+    maps = @client.spots(centerLat, centerLng, :types => ['gym', 'health'], :radius => 5000, :exclude => 'store')
 
     render json: maps
-
 
 
   end
