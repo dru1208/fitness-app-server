8ef4e3c2fa8431e0aa0f771bda33757ef288f7f4
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-09-28 21:28:35 +0000

    fixed rails call to database for finding the most recent date's worth of nutrition information

--- app/controllers/dashboard_nutritions_controller.rb
+++ app/controllers/dashboard_nutritions_controller.rb
@@ -1,12 +1,13 @@
 class DashboardNutritionsController < ApplicationController
   def index
-    calories = UserNutrition.where(:user_id => params[:user_id]).sum(:calories)
-    protein = UserNutrition.where("user_id = " + params[:user_id]).sum(:protein)
-    fat = UserNutrition.where("user_id = " + params[:user_id]).sum(:fat)
-    carbohydrates = UserNutrition.where("user_id = " + params[:user_id]).sum(:carbohydrates)
-    sugar = UserNutrition.where("user_id = " + params[:user_id]).sum(:sugar)
-    sodium = UserNutrition.where("user_id = " + params[:user_id]).sum(:sodium)
-    cholesterol = UserNutrition.where("user_id = " + params[:user_id]).sum(:cholesterol)
+    last_date = UserNutrition.where("user_id = " + params[:user_id].to_s).order('datetime DESC').last['datetime']
+    calories = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime = '#{last_date.to_s}'").sum(:calories)
+    protein = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime = '#{last_date.to_s}'").sum(:protein)
+    fat = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime = '#{last_date.to_s}'").sum(:fat)
+    carbohydrates = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime = '#{last_date.to_s}'").sum(:carbohydrates)
+    sugar = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime = '#{last_date.to_s}'").sum(:sugar)
+    sodium = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime = '#{last_date.to_s}'").sum(:sodium)
+    cholesterol = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime = '#{last_date.to_s}'").sum(:cholesterol)
 
 
 
