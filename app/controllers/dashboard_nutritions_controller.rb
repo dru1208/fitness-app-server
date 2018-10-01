196a0719935d0fddb426523c472ff92e6e6d9358
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-01 08:03:09 +0000

    added conditional if nutrition info does not exist with new account

--- app/controllers/dashboard_nutritions_controller.rb
+++ app/controllers/dashboard_nutritions_controller.rb
@@ -1,30 +1,34 @@
 class DashboardNutritionsController < ApplicationController
   def index
     last_date_time = UserNutrition.where("user_id = " + params[:user_id].to_s).maximum("datetime")
-    last_date_adjusted = last_date_time - (60 * 60 * 24)
+    if last_date_time
+      last_date_adjusted = last_date_time - (60 * 60 * 24)
 
 
-    calories = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:calories)
-    protein = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:protein)
-    fat = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:fat)
-    carbohydrates = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:carbohydrates)
-    sugar = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:sugar)
-    sodium = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:sodium)
-    cholesterol = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:cholesterol)
+      calories = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:calories)
+      protein = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:protein)
+      fat = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:fat)
+      carbohydrates = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:carbohydrates)
+      sugar = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:sugar)
+      sodium = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:sodium)
+      cholesterol = UserNutrition.where("user_id = " + params[:user_id].to_s + " AND datetime >= '#{last_date_adjusted.to_s}'").sum(:cholesterol)
 
 
 
-    nutritionSummary = {
-      calories: calories,
-      protein: protein,
-      fat: fat,
-      carbohydrates: carbohydrates,
-      sugar: sugar,
-      sodium: sodium,
-      cholesterol: cholesterol
-    }
+      nutritionSummary = {
+        calories: calories,
+        protein: protein,
+        fat: fat,
+        carbohydrates: carbohydrates,
+        sugar: sugar,
+        sodium: sodium,
+        cholesterol: cholesterol
+      }
 
-    puts "nutrition summary: " + nutritionSummary.inspect
-    render json: nutritionSummary
+      puts "nutrition summary: " + nutritionSummary.inspect
+      render json: nutritionSummary
+    else
+      render json: []
+    end
   end
 end
